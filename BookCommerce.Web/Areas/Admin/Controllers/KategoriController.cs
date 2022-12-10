55a4a233861c0a3cf9360354384010cd1d2a25dd
Nusret Bilallaj <netktit@gmail.com>
2022-12-10 16:31:58 +0100

    Shtimi i kategorive dhe pjeset tjera

--- BookCommerce.Web/Areas/Admin/Controllers/KategoriController.cs
+++ BookCommerce.Web/Areas/Admin/Controllers/KategoriController.cs
@@ -17,5 +17,87 @@ namespace BookCommerce3.Web.Areas.Admin.Controllers
           List<Kategoria> lista=  _kontekst.Kategorite.ToList();
             return View(lista);
         }
+
+        public IActionResult Krijo()
+        {
+            var obj = new Kategoria();
+            obj.Renditja = _kontekst.Kategorite.Max(x=>x.Renditja)+1;
+            return View(obj);
+        }
+
+        [HttpPost]
+        public IActionResult Krijo(Kategoria kat)
+        {
+            if (ModelState.IsValid)
+            {
+                _kontekst.Kategorite.Add(kat);
+                _kontekst.SaveChanges();
+                TempData["suksesi"] = "U shtua me sukses!";
+                return RedirectToAction("Listo");
+            }
+
+            return View(kat);
+        }
+
+        public IActionResult Ndrysho(int? id)
+        {
+            if (id==null || id==0)
+            {
+                return NotFound();
+            }
+            Kategoria kat= _kontekst.Kategorite.Find(id);
+            if (kat==null)
+            {
+                return NotFound();
+            }
+            return View(kat);
+        }
+
+        [HttpPost]
+        public IActionResult Ndrysho(Kategoria kat)
+        {
+            if (ModelState.IsValid)
+            {
+                _kontekst.Kategorite.Update(kat);
+                _kontekst.SaveChanges();
+                TempData["suksesi"] = "U ndryshua me sukses!";
+                return RedirectToAction("Listo");
+            }
+
+            return View(kat);
+        }
+
+        public IActionResult Fshi(int? id)
+        {
+            if (id == null || id == 0)
+            {
+                return NotFound();
+            }
+            Kategoria kat = _kontekst.Kategorite.Find(id);
+            if (kat == null)
+            {
+                return NotFound();
+            }
+            return View(kat);
+        }
+
+        [HttpPost,ActionName("Fshi")]
+        public IActionResult FshiPo(int? id)
+        {
+            if (id==0 || id==null)
+            {
+                return NotFound();
+            }
+            Kategoria katPrjDb = _kontekst.Kategorite.Find(id);
+            if (katPrjDb==null)
+            {
+                return NotFound();
+            }
+            _kontekst.Kategorite.Remove(katPrjDb);
+            _kontekst.SaveChanges();
+            TempData["suksesi"] = "Eshte fshire me sukses!";
+            return RedirectToAction("Listo");
+        }
+
     }
 }
